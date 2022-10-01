6b94acf6049ee6634126ea965cfda9c0706d05c4
Julián Sanmartino <jjsanmartino03@gmail.com>
2022-10-01 16:19:15 -0300

    initial commit v2

--- helloWorldWind.js
+++
@@ -1,8 +0,0 @@
-const myCanvas = document.getElementById('canvasOne');
-
-myCanvas.setAttribute('width', window.innerWidth);
-myCanvas.setAttribute('height', window.innerHeight);
-
-let wwd = new WorldWind.WorldWindow('canvasOne');
-
-wwd.addLayer(new WorldWind.BMNGOneImageLayer());
